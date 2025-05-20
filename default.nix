f652d399b38a7cb71077d446bb845749b74064c5
Johan Thomsen <joho@nnit.com>
2025-05-20 17:28:47 +0200

    update nixpkgs and add cowsay

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   _pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/9a91318fffec81ad009b73fd3b640d2541d87909.tar.gz";
-    sha256 = "sha256:16j9il4a540ihz16dz8bzsxxqwiiyy1i2c1xvs5y7qm9s6v3dr4b";
+    url = "https://github.com/NixOS/nixpkgs/archive/9b5ac7ad45298d58640540d0323ca217f32a6762.tar.gz";
+    sha256 = "sha256:0zqvvkb1gsqxvm2vvp184spm1kc363sx0byv7cdxmmj4wk51kfv1";
   }) {
     config = {};
     overlays = [(self: super: {
@@ -15,9 +15,12 @@ let
       });
     })];
   };
+
+  genericImage = name: { inherit name; config.EntryPoint = [ "${_pkgs.${name}}/bin/${name}" ]; };
 in
 { pkgs ? _pkgs, ... }:
 {
+  cowsay = genericImage "cowsay";
   date = import ./images/date.nix { inherit pkgs; };
   date-patched = import ./images/date.nix { inherit pkgs; coreutils = pkgs.coreutils-patched; };
   inherit pkgs;
