4d35f81e80cf2a12f099cb3ccefb364b96025124
Johan Thomsen <joho@nnit.com>
2025-05-20 17:42:28 +0200

    try having an image for all packages

--- default.nix
+++ default.nix
@@ -4,24 +4,15 @@ let
     sha256 = "sha256:0zqvvkb1gsqxvm2vvp184spm1kc363sx0byv7cdxmmj4wk51kfv1";
   }) {
     config = {};
-    overlays = [(self: super: {
-      coreutils-patched = super.coreutils.overrideAttrs (oa: {
-        patches = (oa.patches or []) ++ [
-          (self.fetchpatch {
-            url = "https://github.com/johanot/coreutils/pull/1.patch";
-            sha256 = "sha256-JagCpc3f4Lm0HaL/QaOsPTUANdeJYbtwZt/4RttDAoE=";
-          })
-        ];
-      });
-    })];
   };
 
-  genericImage = name: { inherit name; config.EntryPoint = [ "${_pkgs.${name}}/bin/${name}" ]; };
+  genericImage = name: { inherit name; config.EntryPoint = [ "${_pkgs.${name}}/bin/${_pkgs.${name}.meta.mainProgram or name}" ]; };
 in
 { pkgs ? _pkgs, ... }:
-{
-  cowsay = genericImage "cowsay";
-  date = import ./images/date.nix { inherit pkgs; };
-  date-patched = import ./images/date.nix { inherit pkgs; coreutils = pkgs.coreutils-patched; };
-  inherit pkgs;
-}
+(builtins.mapAttrs (n: _: genericImage n) pkgs) // { inherit pkgs; }
+#{
+  #cowsay = genericImage "cowsay";
+  #date = import ./images/date.nix { inherit pkgs; };
+  #date-patched = import ./images/date.nix { inherit pkgs; coreutils = pkgs.coreutils-patched; };
+  #inherit pkgs;
+#}
